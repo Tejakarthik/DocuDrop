3c3a4dfe5d186e67aee846672c02d72d40c25b1f
Tejakarthik <karthik.teja004@gmail.com>
2024-07-17 22:34:23 +0530

    basic ui minimal styling

---
+++ docudrop/src/router/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from '../components /App';
+import Header from '../components /Header';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </div>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
