d51aab57e7642058ef182deb9b09b4782640a76d
Tejakarthik <karthik.teja004@gmail.com>
2024-07-22 22:21:33 +0530

    Update App.js

--- docudrop/src/components /App.js
+++ docudrop/src/components /App.js
@@ -30,6 +30,15 @@ const App = (props) => {
     };
     fileReader.readAsDataURL(uploadedFile);
     setIsPreviewAvailable(uploadedFile.name.match(/\.(jpeg|jpg|png)$/));
+    dropRef.current.style.border = '2px dashed #e9ebeb';
+  };
+
+  const updateBorder = (dragState) => {
+    if (dragState === 'over') {
+      dropRef.current.style.border = '2px solid #000';
+    } else if (dragState === 'leave') {
+      dropRef.current.style.border = '2px dashed #e9ebeb';
+    }
   };
 
   const handleOnSubmit = async (event) => {
@@ -67,7 +76,9 @@ const App = (props) => {
           </Col>
         </Row>
         <div className="upload-section">
-  <Dropzone onDrop={onDrop}>
+  <Dropzone onDrop={onDrop}
+  onDragEnter={() => updateBorder('over')}
+  onDragLeave={() => updateBorder('leave')} >
     {({ getRootProps, getInputProps }) => (
       <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
         <input {...getInputProps()} />
@@ -106,112 +117,3 @@ const App = (props) => {
 };
 
 export default App;
-/*import React, { useState, useRef } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { Form, Row, Col, Button } from 'react-bootstrap';
-
-const App = (props) => {
-  const [file, setFile] = useState(null); // state for storing actual image
-  const [previewSrc, setPreviewSrc] = useState(''); // state for storing previewImage
-  const [state, setState] = useState({
-    title: '',
-    description: ''
-  });
-  const [errorMsg, setErrorMsg] = useState('');
-  const [isPreviewAvailable, setIsPreviewAvailable] = useState(false); // state to show preview only for images
-  const dropRef = useRef(); // React ref for managing the hover state of droppable area
-
-  const handleInputChange = (event) => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
-    });
-  };
-
-  const onDrop = (files) => {
-    const [uploadedFile] = files;
-    setFile(uploadedFile);
-
-    const fileReader = new FileReader();
-    fileReader.onload = () => {
-      setPreviewSrc(fileReader.result);
-    };
-    fileReader.readAsDataURL(uploadedFile);
-    setIsPreviewAvailable(uploadedFile.name.match(/\.(jpeg|jpg|png)$/));
-  };
-
-  const handleOnSubmit = async (event) => {
-    event.preventDefault();
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    multiple: false,
-    accept: 'image/jpeg, image/png'
-  });
-
-  return (
-    <React.Fragment>
-      <Form className="search-form" onSubmit={handleOnSubmit}>
-        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
-        <Row>
-          <Col>
-            <Form.Group controlId="title">
-              <Form.Control
-                type="text"
-                name="title"
-                value={state.title || ''}
-                placeholder="Enter title"
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group controlId="description">
-              <Form.Control
-                type="text"
-                name="description"
-                value={state.description || ''}
-                placeholder="Enter description"
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <div className="upload-section">
-          <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
-            <input {...getInputProps()} />
-            <p>Drag and drop a file OR click here to select a file</p>
-            {file && (
-              <div>
-                <strong>Selected file:</strong> {file.name}
-              </div>
-            )}
-          </div>
-          {previewSrc ? (
-            isPreviewAvailable ? (
-              <div className="image-preview">
-                <img className="preview-image" src={previewSrc} alt="Preview" />
-              </div>
-            ) : (
-              <div className="preview-message">
-                <p>No preview available for this file</p>
-              </div>
-            )
-          ) : (
-            <div className="preview-message">
-              <p>Image preview will be shown here after selection</p>
-            </div>
-          )}
-        </div>
-        <Button className="submit-btn" variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </React.Fragment>
-  );
-};
-
-export default App;*/
