7c82bd4b108b6eb65bcd2138c1e35207abbdad39
Tejakarthik <karthik.teja004@gmail.com>
2024-08-08 22:07:39 +0530

    Error correction
    there was a space between the components and the slas
    h in the import lines

--- docudrop/src/router/AppRouter.js
+++ docudrop/src/router/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from '../components /App';
-import Header from '../components /Header';
+import App from '../components/App';
+import Header from '../components/Header';
 
 const AppRouter = () => (
   <BrowserRouter>
